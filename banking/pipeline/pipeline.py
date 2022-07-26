afbf59b90a05ea7d04de421814ba7f1c51c7bc8a
niladrischaudhury <niladri.schaudhury@gmail.com>
2022-07-26 23:24:38 +0530

    Pipeline with Data Ingestion

--- banking/pipeline/pipeline.py
+++ banking/pipeline/pipeline.py
@@ -20,11 +20,35 @@ class Pipeline:
             raise BankingException(e, sys) from e
 
     
+    def run_pipeline(self):
+        try:
+            # data ingestion
+            data_ingestion_artifact = self.start_data_ingestion()
+
+        except Exception as e:
+            raise BankingException(e, sys) from e
+
+    
     def start_data_ingestion(self) -> DataIngestionArtifact:
         try:
             data_ingestion = DataIngestion(data_ingestion_config = self.config.get_data_ingestion_config())
-
-            data_ingestion.initiate_data_ingestion()
+            return data_ingestion.initiate_data_ingestion()
         
         except Exception as e:
-            raise BankingException(e, sys) from e
+            raise BankingException(e, sys) from e    
+
+
+    def start_data_validation(self):
+        pass
+
+    def start_data_transformation(self):
+        pass
+  
+    def start_model_trainer(self):
+        pass
+
+    def start_model_evaluation(self):
+        pass
+
+    def start_model_pusher(self):
+        pass
